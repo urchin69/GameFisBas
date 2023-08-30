3e50a2311cfbce9490a8a59bf544ead857916439
urchin69 <urchin@onet.plgit config  --global user.email urchin@onet.plgit config  --global user.email urchin@onet.pl>
2023-08-30 18:24:05 +0200

    Add project files.

---
+++ GameFisBas/FisBas.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFisBas
+{
+    class FisBas
+    {
+        public FisBas() //konstruktor
+        {
+
+        }
+
+        public static string  GetNumber()
+        {
+             string mesText ="";
+
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine(" Wpisany tekst nie jest liczbą całkowitą, wpisz liczbę całkowitą.");
+                    continue;
+                }
+                else
+                {
+                    if ((number % 3 == 0) && (number % 5 == 0))
+                    {
+                        mesText = "FisBas";
+                        return mesText;
+                    }
+                    else if (!(number % 3 == 0) && !(number % 5 == 0))
+                    {
+                        mesText = number.ToString();
+                        return mesText;
+                    }
+
+                    else if (number % 3 == 0)
+                    {
+                        mesText = "Fis";
+                        return mesText;
+                    }
+                    else if( number % 5 == 0)
+                    {
+                        mesText = "Bas";
+                        return mesText;
+                    }
+                }
+            }
+
+    
+        }
+
+
+
+
+    }
+}
---
+++ GameFisBas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFisBas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Podaj liczbę całkowitą, program określi!");
+            var number = FisBas.GetNumber();
+            Console.WriteLine(number);
+        }
+    }
+}
