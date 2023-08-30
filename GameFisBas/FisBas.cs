533640815f9748ed0baa120acf47222431b87a0c
urchin69 <urchin@onet.plgit config  --global user.email urchin@onet.plgit config  --global user.email urchin@onet.pl>
2023-08-30 22:30:29 +0200

    After seeing the solution

--- GameFisBas/FisBas.cs
+++ GameFisBas/FisBas.cs
@@ -29,23 +29,25 @@ namespace GameFisBas
                     if ((number % 3 == 0) && (number % 5 == 0))
                     {
                         mesText = "FisBas";
-                        return mesText;
+                        //return mesText;
                     }
                     else if (!(number % 3 == 0) && !(number % 5 == 0))
                     {
                         mesText = number.ToString();
-                        return mesText;
+                        //return mesText;
                     }
                     else if (number % 3 == 0)
                     {
                         mesText = "Fis";
-                        return mesText;
+                        //return mesText;
                     }
                     else if( number % 5 == 0)
                     {
                         mesText = "Bas";
-                        return mesText;
+                        //return mesText;
                     }
+
+                    return mesText;
                 }
             }
         }
